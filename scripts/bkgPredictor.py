caf11dfbec44c81e303fdb3116f7e30bd250a1ad
Brian Amadio <btamadio@pdsf6.nersc.gov>
2016-10-25 12:56:22 -0700

    2D response

--- scripts/bkgPredictor.py
+++ scripts/bkgPredictor.py
@@ -15,10 +15,12 @@ def err(lst):
     std /= float(len(lst)-1)
     return math.sqrt(std)/math.sqrt(len(lst))
 class bkgPredictor:
-    def __init__(self,dressedFileNames,jobName,lumi=35):
+    def __init__(self,dressedFileNames,jobName,lumi=35,templateType=0):
         self.dressedFileNames = dressedFileNames
+        self.nToys = len(self.dressedFileNames)
         self.jobName = jobName
         self.lumi = lumi
+        self.templateType = templateType
         self.MJbins = [0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.40,0.45,0.5,0.6,0.8,1.0,2.0]
         self.nMJbins = len(self.MJbins)-1
         self.MJbinArray = array.array('d',self.MJbins)
@@ -27,9 +29,13 @@ class bkgPredictor:
         self.massBinUp = 2.0
         self.normRegion = (0.2,0.6)
         self.setupOutput()
-        self.loopAndFill()
+        #self.loopAndFill()
     def setupOutput(self):
-        self.outFile = ROOT.TFile.Open('../output_prediction/'+self.jobName+'.root','RECREATE')
+        outFileName = '../output_prediction/'+self.jobName+'.root'
+        if os.path.isfile(outFileName):
+            self.outFile = ROOT.TFile.Open(outFileName,'UPDATE')
+        else:
+            self.outFile = ROOT.TFile.Open(outFileName,'CREATE')
         self.regionList = ['4jVRb0','4jVRb1','4jVRb9','4jVRbU','4jVRbM',
                            '4jSRb0','4jSRb1','4jSRb9','4jSRbU','4jSRbM',
                            '5jVRb0','5jVRb1','5jVRb9','5jVRbU','5jVRbM',
@@ -45,7 +51,6 @@ class bkgPredictor:
         self.profDict_dressNom = {}
         self.profDict_dressUp = {}
         self.profDict_dressDown = {}
-        
         for regionName in self.regionList:
             self.histDict_kin[regionName] = {}
             self.histDict_dressUp[regionName] = {}
@@ -78,8 +83,37 @@ class bkgPredictor:
                     self.histDict_dressUp[regionName][histType] = ROOT.TH1F(histName_dressUp,histName_dressUp,self.nMassBins,self.massBinLow,self.massBinUp)
                     self.histDict_dressNom[regionName][histType] = ROOT.TH1F(histName_dressNom,histName_dressNom,self.nMassBins,self.massBinLow,self.massBinUp)
                     self.histDict_dressDown[regionName][histType] = ROOT.TH1F(histName_dressDown,histName_dressDown,self.nMassBins,self.massBinLow,self.massBinUp)
+    def plotResponse(self):
+        f = ROOT.TFile.Open(self.dressedFileNames[0])
+        t = f.Get('miniTree')
 
+        nBinsX = 4
+        xLow = 0
+        xUp = 2.0
 
+        nBinsY = 10
+        yLow = -0.7
+        yUp = 0.5
+        
+        nBinsZ = 20
+        zLow = 0.0
+        zUp = 2.0
+
+        if self.templateType == 0:
+            print 'template type 0: pT vs. eta'
+            self.hist3D_dress = ROOT.TH3F('h_jetmass_dress_3D','h_dress',nBinsX,xLow,xUp,nBinsY,yLow,yUp,nBinsZ,zLow,zUp)
+            self.hist3D_kin = ROOT.TH3F('h_jetmass_kin_3D','h_kin',nBinsX,xLow,xUp,nBinsY,yLow,yUp,nBinsZ,zLow,zUp)
+            t.Draw('jet_m_dressed_nom:log10(jet_pt):abs(jet_eta)>>h_jetmass_dress_3D','njet>=4','goff')
+            t.Draw('jet_m:log10(jet_pt):abs(jet_eta)>>h_jetmass_kin_3D','njet>=4','goff')
+        elif self.templateType == 1:
+            print 'template type 1: pT vs. BDT'
+            self.hist3D_dress = ROOT.TH3F('h_jetmass_dress_3D','h_jetmass_dress_3D',nBinsX,xLow,xUp,nBinsY,yLow,yUp,nBinsZ,zLow,zUp)
+            self.hist3D_kin = ROOT.TH3F('h_jetmass_kin_3D','h_jetmass_kin_3D',nBinsX,xLow,xUp,nBinsY,yLow,yUp,nBinsZ,zLow,zUp)
+            t.Draw('jet_m_dressed_nom:log(jet_pt):BDTG>>h_jetmass_dress_3D','njet>=4','goff')            
+            t.Draw('jet_m:log(jet_pt):BDTG>>h_jetmass_kin_3D','njet>=4','goff')            
+        self.outFile.cd()
+        self.hist3D_dress.Write()
+        self.hist3D_kin.Write()
     def loopAndFill(self):
         #loop over pseudoexperiment files and fill prediction histograms
         #first we just copy the kinematic histogram over from the input file, rebinning the MJ histogram
@@ -95,14 +129,14 @@ class bkgPredictor:
                 self.histDict_kin[regionName][histType].Write()
         i=0
         for fileName in self.dressedFileNames:
-#            if i > 100:
-#                continue
+            if i > 100:
+                continue
             if i%100 == 0:
                 print 'processing toy number %i' % i
-            i+=1
             f = ROOT.TFile.Open(fileName)
             if not f:
                 continue
+            i+=1
             for regionName in self.regionList:
                 for histType in self.histList:
                     h_dressUp = f.Get('h_'+histType+'_dressUp_'+regionName)
@@ -116,7 +150,7 @@ class bkgPredictor:
                         self.profDict_dressUp[regionName][histType][bin-1].append(h_dressUp.GetBinContent(bin))
                         self.profDict_dressNom[regionName][histType][bin-1].append(h_dressNom.GetBinContent(bin))
                         self.profDict_dressDown[regionName][histType][bin-1].append(h_dressDown.GetBinContent(bin))
-#        pprint.pprint(self.profDict_dressNom['4jSRb9']['MJ'])
+
         for regionName in self.regionList:
             for histType in self.histList:
                 for bin in range(1,self.histDict_dressNom[regionName][histType].GetNbinsX()+1):
--- scripts/runBkgPred.py
+++ scripts/runBkgPred.py
@@ -2,15 +2,17 @@
 import ROOT,os,array,pprint, math,time,glob,sys
 from bkgPredictor import bkgPredictor
 
-name ='data_bdt'
-dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
-os.system('mkdir -p ../output_prediction/')
-p = bkgPredictor(dressedFileNames,name,1.0)
+#name ='data_bdt'
+#dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
+#os.system('mkdir -p ../output_prediction/')
+#p = bkgPredictor(dressedFileNames,name,1.0,0)
 
 name = 'data_eta'
 dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
-p2 = bkgPredictor(dressedFileNames,name,1.0)
+p2 = bkgPredictor(dressedFileNames,name,1.0,0)
+#p2.loopAndFill()
+p2.plotResponse()
 
-name = 'data_ichep'
-dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
-p3 = bkgPredictor(dressedFileNames,name,1.0)
+#name = 'data_ichep'
+#dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
+#p3 = bkgPredictor(dressedFileNames,name,1.0,2)
