e20b7621ed0b91e94fd0617d0dd20595234e9599
Brian Amadio <btamadio@gmail.com>
2016-10-26 11:02:20 -0700

    template plots and switch for mass corrections

--- scripts/runBkgPred.py
+++ scripts/runBkgPred.py
@@ -8,11 +8,11 @@ p0 = bkgPredictor(dressedFileNames,name,1.0,0)
 p0.loopAndFill()
 p0.plotResponse()
 
-name ='data_bdt'
-dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
-p2 = bkgPredictor(dressedFileNames,name,1.0,1)
-p2.loopAndFill()
-p2.plotResponse()
+#name ='data_bdt'
+#dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
+#p2 = bkgPredictor(dressedFileNames,name,1.0,1)
+#p2.loopAndFill()
+#p2.plotResponse()
 
 #name = 'data_ichep'
 #dressedFileNames = glob.glob('../output_dressed/'+name+'/*.root')
