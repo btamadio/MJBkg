c0eb8b87b84dd6a16b5e637ab45e7bc3adf21c30
Brian Amadio <btamadio@gmail.com>
2016-12-01 11:19:42 -0800

    calculate correction factors

--- scripts/getCorrections.py
+++ scripts/getCorrections.py
@@ -1,58 +1,69 @@
 #!/usr/bin/env python
-import argparse,ROOT,os,math,glob
+import argparse,ROOT,os,math,glob,pprint
 parser = argparse.ArgumentParser(add_help=False, description='get correction factors and uncertainties')
-parser.add_argument('inDir')
+parser.add_argument('inFileName')
 args = parser.parse_args()
-inFileList = [ROOT.TFile.Open(fileName) for fileName in glob.glob(args.inDir+'/*')]
+inFile = ROOT.TFile.Open(args.inFileName)
 
-etaList = ['cen','for']
-regionList = ['4jVRbM','4jVRbU']
-
-def mean(lst):
-    return sum(lst) / float(len(lst))
-kinProf = []
-dressProf = []
-dressAvg = []
+etaList = ['eta1','eta2','eta3','eta4']
+regionList = ['4js1bU']
 
 hKin = {}
+hDress = {}
 kinProf = {}
 dressProf = {}
 kinErr = {}
-dressAvg = {}
+corrections = {}
+finalCorrections = {}
+
 for eta in etaList:
     for region in regionList:
         key = eta+'_'+region
-        hKin[key] = inFileList[0].Get('h_prof1d_'+eta+'_kin_'+region).ProjectionX()
+        hKin[key] = inFile.Get('h_avgMass_'+eta+'_kin_'+region)
+        hDress[key] = inFile.Get('h_avgMass_'+eta+'_dress_'+region)
         kinProf[key] = []
         dressProf[key] = []
         kinErr[key]=[]
         for bin in range(1,hKin[key].GetNbinsX()+1):
             kinProf[key].append(hKin[key].GetBinContent(bin))
             kinErr[key].append(hKin[key].GetBinError(bin))
-            dressProf[key].append([])
-i=0
-for inFile in inFileList:
-    for eta in etaList:
-        for region in regionList:
-            key = eta+'_'+region
-            hDress = inFile.Get('h_prof1d_'+eta+'_dress_'+region).ProjectionX('prof_'+str(i))
-            for bin in range(1,hDress.GetNbinsX()+1):
-                dressProf[key][bin-1].append(hDress.GetBinContent(bin))
-            i+=1
+            dressProf[key].append(hDress[key].GetBinContent(bin))
 
 for eta in etaList:
     for region in regionList:
-        print eta,region
         key = eta+'_'+region
-        dressAvg[key] = []
-        uncert = 0.0
+        corrections[key] = []
+        finalCorrections[key] = []
         for i in range(len(dressProf[key])):
-            dressAvg[key].append(mean(dressProf[key][i]))
-            corr = (kinProf[key][i]-dressAvg[key][i])/dressAvg[key][i]
-            err = kinErr[key][i]/dressAvg[key][i]
-            if abs(corr) < err:
-                if abs(corr) > uncert:
-                    uncert = abs(corr)
-                corr = 0.0
-            print corr
-        print 'uncert = ',uncert
+            corr = (kinProf[key][i]-dressProf[key][i])/dressProf[key][i]
+            err = kinErr[key][i]/dressProf[key][i]
+            corrections[key].append(corr)
+            finalCorrections[key].append(0)
+
+for key in corrections:
+    print key
+    for i in range(len(corrections[key])):
+        doCorrection = False
+        if abs(corrections[key][i]) > (kinErr[key][i] / dressProf[key][i]):
+            thisSign = math.copysign(1,corrections[key][i])
+            if i == 0:
+                if(abs(corrections[key][i+1]) > (kinErr[key][i+1] / dressProf[key][i+1])):
+                    if math.copysign(1,corrections[key][i+1]) == thisSign:
+                        doCorrection = True
+            elif i == len(corrections[key]) - 1:
+                if(abs(corrections[key][i-1]) > kinErr[key][i-1] / dressProf[key][i-1]):
+                    if math.copysign(1,corrections[key][i-1]) == thisSign:
+                        doCorrection = True
+            elif (abs(corrections[key][i+1]) > kinErr[key][i+1] / dressProf[key][i+1]) and (abs(corrections[key][i-1]) > kinErr[key][i-1] / dressProf[key][i-1]):
+                if math.copysign(1,corrections[key][i-1]) == thisSign and math.copysign(1,corrections[key][i+1]) == thisSign:
+                    doCorrection = True
+            if doCorrection:
+                finalCorrections[key][i] = corrections[key][i]
+                if i > 0:
+                    finalCorrections[key][i-1] = corrections[key][i-1]
+                if i < len(corrections[key])-1:
+                    finalCorrections[key][i+1] = corrections[key][i+1]
+for key in corrections:
+    print key
+    for i in range(len(corrections[key])):
+        print finalCorrections[key][i]
